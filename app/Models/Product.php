3e17c1c92ef3a563131d81befd3c029cd8ae3ec6
msunil1992 <sunil.m@geniusits.com.au>
2021-12-08 14:56:23 +1100

    pack a box app created with product and box classes

---
+++ app/Models/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillables = [
+        'name',
+        'length',
+        'width',
+        'height',
+    ];
+
+    /**
+     *  box
+     *
+    */
+    public function box()
+    {
+        $this->belongsTo(Box::class);
+    }
+
+}
